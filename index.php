7ca787ec015dda1117c616a9d52fb2128068fa74
Fabio Alessandro Locati <f.locati@snds.co>
2012-01-25 21:05:57 +0100

    Now date works properly :)

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
 			<div id="post-<?php the_ID() ?>" class="<?php sandbox_post_class() ?>">
 				<h2 class="entry-title"><a href="<?php the_permalink() ?>" title="<?php printf( __('Permalink to %s', 'sandbox'), the_title_attribute('echo=0') ) ?>" rel="bookmark"><?php the_title() ?></a></h2>
                                 <?php if( is_single() ): ?>
-                                    <div class="entry-date"><abbr class="published" title="<?php the_time('Y-m-d\TH:i:sO') ?>"><?php unset($previousday); printf( __( '%1$s &#8211; %2$s', 'sandbox' ), the_date( '', '', '', false ), get_the_time() ) ?></abbr></div>
+                                    <div class="entry-date"><?php the_time('Y-m-d H:i:s') ?></div>
                                 <?php endif; ?>
 				<div class="entry-content">
 <?php the_content( __( 'Read More <span class="meta-nav">&raquo;</span>', 'sandbox' ) ) ?>
