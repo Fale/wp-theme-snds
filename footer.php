ed78a09b541cbe74ca24251ef334e33d6dde26c9
Fabio Alessandro Locati <f.locati@snds.co>
2012-02-11 22:06:34 +0100

    Improve the footer

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
 
     <div id="footer">
         <div id="content">
-            &copy; Fabio Alessandro Locati<br />
+            &copy; 2012 - Fabio Alessandro Locati<br />
         </div>
     </div><!-- #footer -->
 
